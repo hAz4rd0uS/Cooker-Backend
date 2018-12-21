5e54eda20effabe839abcf69b0184355fa6740d3
Pierrick MARTELLIERE <p.martelliere@gmail.com>
2018-12-21 17:57:08 +0100

    .

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -7,7 +7,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-11-03T16:01:50.482Z",
+    "time": "2018-12-21T16:48:14.402Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
